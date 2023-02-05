58e43cad98227fd5c1efe05ab23b7e883a7a98d8
jaeseonNamgung <sunnamgung8@naver.com>
2023-02-05 17:24:51 +0900

    페이징

--- src/test/java/com/study/querydsl/QuerydslBasicTest.java
+++ src/test/java/com/study/querydsl/QuerydslBasicTest.java
@@ -128,4 +128,22 @@ public class QuerydslBasicTest {
         assertThat(member6.getUsername()).isEqualTo("member6");
         assertThat(memberNull.getUsername()).isNull();
     }
+    
+    @Test
+    void paging(){
+        List<Member> result = queryFactory.selectFrom(member)
+                .orderBy(member.username.desc())
+                .offset(1)
+                .limit(2)
+                .fetch();
+        assertThat(result.size()).isEqualTo(2);
+    }
+    @Test
+    void paging2(){
+        queryFactory.selectFrom(member.count())
+        ;
+
+
+        assertThat(result.size()).isEqualTo(2);
+    }
 }
